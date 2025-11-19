f904719f4fd4728409028ad196cad3f364c418ad
garethrogers2828 <rogers2828@icloud.com>
2025-11-19 19:12:28 +0000

    Display addition question to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,42 @@
- //Wait for the DOM to finish loading before running the game
- // Get the button elements and add events listeners to them
+//Wait for the DOM to finish loading before running the game
+// Get the button elements and add events listeners to them
 
- document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
-        button.addEventListener("click", function() {
+        button.addEventListener("click", function () {
             if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
- })
 
- /**
-  * The main game "loop", called when the script is first loaded 
-  * and after the user's anser has been processed
-  */
+    runGame("addition");
 
- function runGame() {
+})
+
+/**
+ * The main game "loop", called when the script is first loaded 
+ * and after the user's anser has been processed
+ */
+
+function runGame(gameType) {
 
     //creates 2 random numbers between 1 and 24
-    let num1 = Math.floor(Math.random() *25) + 1;
-    let num2 = Math.floor(Math.random() *25) + 1;
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -43,7 +54,11 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+
+    document.getElementById(`operand1`).textContent = operand1;
+    document.getElementById(`operand2`).textContent = operand2;
+    document.getElementById(`operator`).textContent = "+";
 
 }
 
